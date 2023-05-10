d28cd62faacaa34e3ac9058f7560b4eef5c033e9
70gaurav <gk17265@gmail.com>
2023-05-10 22:34:21 +0530

    index commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Home } from './ecommerce/Home';
-// import {Todo} from './To do list/todo';
-// import Home from './tesla/Home';
-// import Axios from './axios/Axios';
-// import Tabs from './tabs/tabs';
-// import App from './Routing/App';
+
 import Movie from './movie finder/movie';
-// import App from './Portfolio/App'
-// import App from './fakestore.api/App';
-// import App from './Confronto/App';
-// import Table from './table/Table';
+
 
 
 
@@ -18,11 +10,7 @@ import Movie from './movie finder/movie';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(<Movie />)
-// root.render(<Table />)
-// root.render(<Todo />)
-// root.render(<Home />)
-// root.render(<Tabs />)
-// root.render(<App />)
+
 
  
 
